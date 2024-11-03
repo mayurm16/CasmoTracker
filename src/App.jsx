a1345e008664a3442281776cb2432870a73bfc9d
mayurm16 <m47.mayur@gmail.com>
2024-11-03 17:45:02 +0530

    initialCommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Added ts and tsx for TypeScript support if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
